82e0a594aa0fb17a09a397d78ee69b82c3ad1e1c
Paulwatchful <99956519+Paulwatchful@users.noreply.github.com>
2024-06-11 17:01:19 +0100

    word and view email

--- myproject/reports/urls.py
+++ myproject/reports/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('emails/', views.email_list, name='email_list'),
     path('emails/download/<str:message_id>/<str:attachment_id>/', views.download_attachment_view, name='download_attachment'),
+    path('emails/view/<str:message_id>/<str:attachment_id>/', views.view_pdf, name='view_pdf'),
     path('', views.index, name='index'),  # Handle the root /reports/ URL
 ]
