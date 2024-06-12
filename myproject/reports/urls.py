557ef9b870bd10b30676c44e39e74986e7ff64e6
Paulwatchful <99956519+Paulwatchful@users.noreply.github.com>
2024-06-12 11:44:28 +0100

    ui complete

--- myproject/reports/urls.py
+++ myproject/reports/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('emails/', views.email_list, name='email_list'),
     path('emails/download/<str:message_id>/<str:attachment_id>/', views.download_attachment_view, name='download_attachment'),
     path('emails/view/<str:message_id>/<str:attachment_id>/', views.view_pdf, name='view_pdf'),
-    path('serve_pdf/<str:filename>/', views.serve_pdf, name='serve_pdf'),  # Add this line
-    path('', views.index, name='index'),  # Handle the root /reports/ URL
+    path('serve_pdf/<str:filename>/', views.serve_pdf, name='serve_pdf'),
+    path('', views.index, name='index'),
 ]
