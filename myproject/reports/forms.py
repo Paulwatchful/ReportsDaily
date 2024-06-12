781c4eaa9e5291c248e8ceba58c7d86d252177e2
Paulwatchful <99956519+Paulwatchful@users.noreply.github.com>
2024-06-12 13:30:20 +0100

    email forward

--- myproject/reports/forms.py
+++ myproject/reports/forms.py
@@ -5,18 +5,35 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ['name']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
 class EmailTemplateForm(forms.ModelForm):
     class Meta:
         model = EmailTemplate
         fields = ['project', 'subject', 'body']
+        widgets = {
+            'project': forms.Select(attrs={'class': 'form-control'}),
+            'subject': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }
 
 class RecipientForm(forms.ModelForm):
     class Meta:
         model = Recipient
         fields = ['project', 'email']
+        widgets = {
+            'project': forms.Select(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        }
 
 class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
         fields = ['title', 'content', 'project']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'project': forms.Select(attrs={'class': 'form-control'}),
+        }
